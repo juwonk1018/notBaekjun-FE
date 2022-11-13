fca4c35301c3ddcf05d3efc016a2afb5fa597641
Jinnieeeey <115052493+Jinnieeeey@users.noreply.github.com>
2022-11-13 21:11:53 +0900

    Add files via upload

--- src/page/login.js
+++ src/page/login.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import {Button} from "@material-ui/core";
+import "./login.css"
 
 function Login() {
 
-    return (
-      <Button>
-        Login Page
-      </Button>   
-    );
+  return (
+    <div id="login-form-wrap">
+      <h2>Login</h2>
+      <form id="login-form">
+        <p>
+          <input type="text" id="username" name="username" placeholder="Username" required/>
+        </p>
+        <p>
+          <input type="email" id="email" name="email" placeholder="Email Address" required/>
+        </p>
+        <p>
+          <input type="submit" id="login" value="Login"></input>
+        </p>
+      </form>
+      <div id="create-account-wrap">
+        <p>Not a member? <a href="./signup.js">Create Account</a></p>
+      </div>
+    </div>
+  );
   }
 
   export default Login;
---
+++ src/page/signup.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import "./signup.css"
+
+function Login() {
+
+  return (
+    <div class="form_wrapper">
+        <div class="form_container">
+            <div class="title_container">
+                <h2>Sigan up</h2>
+            </div>
+            <div class="row clearfix">
+                <div>
+                    <form>
+                        <div class="input_field">
+                            <input type="email" name="email" placeholder="Email" required />
+                        </div>
+                        <div class="input_field">
+                            <input type="password" name="password" placeholder="Password" required />
+                        </div>
+                        <div class="input_field">
+                            <input type="password" name="password" placeholder="Re-type Password" required />
+                        </div>
+                        <div class="row clearfix">
+                            <div class="col_half">
+                                <div class="input_field">
+                                    <input type="text" name="name" placeholder="First Name" />
+                                </div>
+                            </div>
+                            <div class="col_half">
+                                <div class="input_field">
+                                    <input type="text" name="name" placeholder="Last Name" required />
+                                </div>
+                            </div>
+                        </div>
+            	        <div class="radio_option">
+                            <input type="radio" name="radiogroup1" id="rd1"/>
+                            <label>Male</label>
+                            <input type="radio" name="radiogroup1" id="rd2"/>
+                            <label>Female</label>
+                        </div>  
+
+                        <div class="checkbox_option">
+                            <input type="checkbox" id="cb1"/>
+                            <label for="cb1">I agree with terms and conditions</label>
+                        </div>
+                        <div class="checkbox_option">
+                            <input type="checkbox" id="cb2"/>
+                            <label for="cb2">I want to receive the newsletter</label>
+                        </div>
+                        <input class="button" type="submit" value="Register" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+  }
+
+  export default Login;
