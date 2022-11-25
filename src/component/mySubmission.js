a4a3c7ad3a4bf56b6086071de75ccd53d5a84b92
Jinnieeeey <115052493+Jinnieeeey@users.noreply.github.com>
2022-11-25 15:26:09 +0900

    Add files via upload

--- src/component/mySubmission.js
+++ src/component/mySubmission.js
@@ -1,12 +1,50 @@
 import React, {useState} from "react";
+import DragDrop from './dragDrop.js';
 import { Button } from "@material-ui/core";
 import "./mySubmission.css"
+
+
+
 
 function MySubmission() {
     
     return (
       <div>
-      </div>   
+        <div className = "wrap">
+          <div className = "inputDiv">
+            <textarea className = "input" placeholder = 'Your Code'>
+            </textarea>
+            <div className = "inputWrap">
+              <form>
+                
+                
+              </form>
+            </div>
+            
+          </div>
+          <div className = "option">
+            <div id = "uploadedFiles">
+                <div className = 'fileDiv'>
+                  <input type = "file" id = "fileInput" name = "filename" multiple = "multiple">
+                  
+                  </input>
+                  <label className = "fileInputButton" for = "fileInput">
+                    Upload
+                  </label>
+                  
+                </div>
+                
+            </div>
+          </div>
+        
+        </div>
+        <div className = "outputDiv">
+            <div className = "outputTitle">
+              <h4>Output</h4>
+            </div>
+            <div className = "output"></div>
+        </div>
+       </div>  
     );
   }
 
